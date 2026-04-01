71bf3bc2fb3d86c7a51125833798cb34d5f76591
Sebastian Wilke <sebastian@wilke.id>
2026-04-01 19:23:17 +0000

    Update domain const

--- custom_components/anytype_todo/const.py
+++ custom_components/anytype_todo/const.py
@@ -4,7 +4,7 @@ from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "anytype"
+DOMAIN = "anytype_todo"
 ATTRIBUTION = "Data provided by Anytype API"
 
 # Configuration constants
